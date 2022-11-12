59b4207db6031ee8cfb6debb1fe3ed8fc63b5ff3
Gabriel Rodrigues <gabrielrodrigues1927@gmail.com>
2022-11-11 21:57:34 -0300

    Ajuste mordal

--- components/Modal.js
+++ components/Modal.js
@@ -37,36 +37,39 @@ const Modal = ({ id='modal',onClose = () => {} }) => {
     
 return(
 
+    
         <div id={id} onClick={handleOutsideClick} className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-md flex justify-center items-center flex-col ">
-        <h3 className="text-4xl my-3 text-gray-400 text-center">Mande alguma proposta, ou apenas um feedback</h3>
+        <div className=" bg-black justify-center items-center p-10 flex flex-col rounded-md">
+        <h3 className="text-4xl my-3 text-gray-400 text-center">Contato</h3>
         <section className="mt-16 ">
             
-            <form className=' w-96 flex flex-col space-y-12 text-gray-400 bg-transparent font-semibold' onSubmit={sendEmail}>
+            <form className=' w-80 flex flex-col space-y-12 text-gray-400 bg-transparent font-semibold' onSubmit={sendEmail}>
                 <input 
                     className="py-1 outline-none bg-transparent border-gray-600 border-b-2 focus:border-purple-600 duration-300 focus:outline-none"
                     type="text"
-                    placeholder="Digite seu nome"
+                    placeholder="seu nome"
                     onChange={(e) => setName(e.target.value)}
                     value={name}
                 />
                 <input 
                     className="py-1 outline-none bg-transparent border-gray-600  border-b-2 focus:border-purple-600 duration-300 focus:outline-none"
                     type="text"
-                    placeholder="Digite seu email"
+                    placeholder="seu email"
                     onChange={(e) => setEmail(e.target.value)}
                     value={email}
                 />
                 <textarea 
                     className="py-0 outline-none bg-transparent border-gray-600  border-b-2 focus:border-purple-600 duration-300 focus:outline-none "
-                    placeholder="Digite sua mensagem..."
+                    placeholder="sua mensagem..."
                     onChange={(e) => setMessage(e.target.value)}
                     value={message}
                 />
-                <input className=" bg-gray-400 text-white px-6 p-2 rounded-md hover:ring-2 hover:ring-gray-300 cursor-pointer" 
+                <input className=" bg-purple-600 text-white px-6 p-2 rounded-md hover:ring-2 hover:ring-gray-300 cursor-pointer" 
                 type="submit" value="Enviar" />
             </form>
         </section>
         </div>
+    </div>
 
     )
 }
