2a178dd38736f14f1471e9e5ed9f2e71ef941de6
Gabriel Rodrigues <gabrielrodrigues1927@gmail.com>
2022-11-13 11:40:19 -0300

    Ajuste

--- sections/Hero.js
+++ sections/Hero.js
@@ -14,14 +14,13 @@ const Hero = () => {
             <h1 className="text-7xl font-bold">Eu sou o 
             <span className="dark:text-purple-600"> Gabriel</span>
             </h1>
-            <h3 className="text-4xl my-3"> um desenvolvedor web</h3>
-            <p className="text-gray-500 mb-8 "> Sou um grande apaixonado por tecnologia e esse mundo fascinante,
-            sou vidrado em resolver problemas e descobrir novas tecnologias ou novos jeitos de fazer coisas simples."
+            <h3 className="text-4xl my-3"> um desenvolvedor</h3>
+            <p className="text-gray-500 mb-8 "> "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
             </p>
             <Button className="bg-purple-600 text-white px-6" onClick={() => setIsModalVisible(true)}> 
                 Contato
             </Button>
-            
+
             {isModalVisible ? <Modal onClose={() => setIsModalVisible(false)}></Modal> :null} 
         
         </section>
