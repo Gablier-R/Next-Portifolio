fe749887a01ab4c4ea9e2d4104819c0ec912f8e0
Gabriel Rodrigues Pereira Rocha <100489145+Gablier-R@users.noreply.github.com>
2022-12-19 17:42:47 -0300

    Ajuste

--- components/Sobre.js
+++ components/Sobre.js
@@ -1,3 +1,18 @@
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from "react";
 
 function Sobre () {
@@ -7,9 +22,9 @@ function Sobre () {
             <li class="mb-10 ml-4">
                 <div class="absolute w-3 h-3 bg-purple-600 rounded-full mt-1.5 -left-1.5 border border-purple-600 "></div>
                 <time class="mb-1 text-sm font-normal leading-none text-gray-500">Agosto 2017</time>
-                <h3 class="text-lg font-bold text-gray-900 dark:text-gray-200">Instituto Federal Goiano - Campus Posse</h3>
+                <h3 class="text-lg font-bold text-gray-900 dark:text-gray-200 ">Instituto Federal Goiano - Campus Posse</h3>
                 <p class="text-base font-normal text-gray-500">
-                Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                 <span className="italic">Estudande</span> do curso de Tecnico em Informatica, focado totalemente no mercado de trabalho, os estudos são aplicados essencialmente a programação e manutenção de maquinas.
                 </p>
             </li>
             <li class="mb-10 ml-4">
@@ -17,7 +32,7 @@ function Sobre () {
                 <time class="mb-1 text-sm font-normal leading-none text-gray-500">Fevereiro 2019</time>
                 <h3 class="text-lg font-bold text-gray-900 dark:text-gray-200">Universidade Estadual de Goiás - UnU Posse</h3>
                 <p class="text-base font-normal text-gray-500">
-                Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                <span className="italic">Estudande</span> do curso de Sistemas de Informação, aprofundando ainda mais nos conhecimentos em programação e uma experiencia magnifica nos estudos academicos.
                 </p>
             </li>
             <li class="mb-10 ml-4">
@@ -25,7 +40,7 @@ function Sobre () {
                 <time class="mb-1 text-sm font-normal leading-none text-gray-500">Agosto 2022</time>
                 <h3 class="text-lg font-bold text-gray-900 dark:text-gray-200">Wom System</h3>
                 <p class="text-base font-normal text-gray-500">
-                Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                Atuando como <span className="italic">Programador junior</span>, tendo atividades como desenvolvimento, refatoração de codigos e documentações nos devidos padrões.
                 </p>
             </li>
         </ol>
@@ -35,3 +50,4 @@ function Sobre () {
 }
 
 export default Sobre
+
--- pages/projects.js
+++ pages/projects.js
@@ -1,11 +1,14 @@
-
+import React,{useState} from "react"
+import Modal from "../components/Modal"
 
 const Projects = () => {
-    return(
-        <section className="mt-16">
-            <div className="text-center text-gray-600 front-semibold mt-20 text-2xl">em desenvolvimento</div>
 
-            
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    return(
+        <section className="mt-16 ">
+            <div className=" font-bold hover:text-purple-600 dark:hover:text-purple-600 underline text-center  mt-20 text-2xl cursor-pointer text-gray-900 dark:text-gray-200 transition ease-in-out delay-150" onClick={() => setIsModalVisible(true)}>Vamos trabalhar juntos...</div>
+            {isModalVisible ? <Modal onClose={() => setIsModalVisible(false)}></Modal> :null} 
         </section>
         
     )
