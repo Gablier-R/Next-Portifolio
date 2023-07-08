83292ebf36409834d7fbf3126472372a4672e662
Gabriel Rodrigues <gabrielrodrigues1927@gmail.com>
2023-07-08 10:43:09 -0300

    Alter about

--- sections/Hero.js
+++ sections/Hero.js
@@ -1,7 +1,7 @@
 
 import Button from "../components/Button"
 
-import React,{useState} from "react"
+import React, { useState } from "react"
 import Modal from "../components/Modal"
 
 const Hero = () => {
@@ -9,20 +9,20 @@ const Hero = () => {
     const [isModalVisible, setIsModalVisible] = useState(false);
 
     return (
-        
+
         <section className="mt-16">
-            <h1 className="text-7xl font-bold">Eu sou o 
-            <span className="dark:text-purple-600"> Gabriel</span>
+            <h1 className="text-7xl font-bold">Eu sou o
+                <span className="dark:text-purple-600"> Gabriel </span>
             </h1>
             <h3 className="text-4xl my-3"> um desenvolvedor</h3>
             <p className="text-gray-500 mb-8"> "Oi eu sou <span className="italic" >@gablier_r</span> , e trabalho com desenvolvimento web entre outros, sou um programador no inicio da sua carreria, e apaixonado pelas minhas criações."
             </p>
-            <Button className="bg-purple-600 text-white px-6" onClick={() => setIsModalVisible(true)}> 
+            <Button className="bg-purple-600 text-white px-6" onClick={() => setIsModalVisible(true)}>
                 Contato
             </Button>
 
-            {isModalVisible ? <Modal onClose={() => setIsModalVisible(false)}></Modal> :null} 
-        
+            {isModalVisible ? <Modal onClose={() => setIsModalVisible(false)}></Modal> : null}
+
         </section>
     )
 }
